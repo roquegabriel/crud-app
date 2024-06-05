188e5b1480dd62824fb6500868a15e9022248230
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-05 14:19:10 -0400

    Change the second argument

--- conn.php
+++ conn.php
@@ -8,8 +8,7 @@ try {
     $password = "1986";
 
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
