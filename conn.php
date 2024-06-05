6d4888701c9417defbde11907ae112c16ab6459b
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-05 14:33:29 -0400

    Format code

--- conn.php
+++ conn.php
@@ -6,7 +6,6 @@ try {
     $dbname = "todo";
     $user = "root";
     $password = "1986";
-
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
