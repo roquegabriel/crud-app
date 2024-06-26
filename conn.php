e46a6532888e398de38e8ef171f4be221284b7d6
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-26 19:17:26 -0400

    Update connection setting

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ try {
     $host = "localhost";
     $dbname = "todo";
     $user = "root";
-    $password = "1986";
+    $password = "";
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
